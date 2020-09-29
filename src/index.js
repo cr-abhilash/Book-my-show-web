63b97a5db8879974a3fc14b7349c848faa50b874
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 01:32:02 +0530

    imported the store file inside index component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
+import store from '../src/store'
 
 ReactDOM.render(
   <Provider store={store}>
