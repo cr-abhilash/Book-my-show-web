76a27fbe841ea034d0227a8b4dd9eb1edc348b8b
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 01:14:14 +0530

    renames the tutorial to event and added some comments inside reventListReducer file

--- src/reducer/eventListReducer.js
+++ src/reducer/eventListReducer.js
@@ -1,43 +1,49 @@
 const initialState={
-    tutorials: [],
-      currentTutorial: null,
+    events: [],
+      currentEvent: null,
       currentIndex: -1,
       searchTitle: ""
 }
 
-const eventListReducer=(state=initialState,action)=>{
+const eventListReducer =(state=initialState,action)=>{
     switch(action.type){
+    //  when dom loaded on eventlist
     case 'GET_ALL':
     {
     return{...state,
-        tutorials: action.value
+        events: action.value
     }
 }
+//    when button gets clicked and then passed the search bar value 
     case 'GET_BY_TITLE':
     {
     return{...state,
-        tutorials: action.value
+        events: action.value
     }
 }
 
+// when clicked on removeall button
 case 'DELETE_ALL':
     {
     return{...state,
-        tutorials: action.value
-    }
-}
-case 'CHANGE_TUTORIAL':{
-    return{...state,
-        currentTutorial: action.value
+       events: action.value
     }
 }
 
+// when clicked on any event, set the information about that event inside currentTutorial
+case 'CHANGE_EVENT':{
+    return{...state,
+        currentEvent: action.value
+    }
+}
+// when clicked on any event, set the index of that event inside currentIndex
 case 'CHANGE_INDEX':{
     return{...state,
         currentIndex: action.value
     }
 }
 
+// Onchange search bar input value
 case 'SEARCH_TITLE':{
     return{...state,
         searchTitle: action.value
