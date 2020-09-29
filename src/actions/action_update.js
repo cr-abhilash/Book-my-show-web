e93454b89f3f5e42b6c3a95d556682381fcfadeb
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 00:44:48 +0530

    rename imported tutorialDataService to EventDataService inside action_update file

--- src/actions/action_update.js
+++ src/actions/action_update.js
@@ -1,78 +1,72 @@
-import TutorialDataService from "../services/tutorial.service";
+import EventDataService from "../services/eventService";
 
-
-export const getTutorialValue=(id)=>{
-    return(dispatch)=>{
-        TutorialDataService.get(id)
-      .then(response => {
-        console.log(response.data,"action");
-        dispatch({type:"GET_TUTORIAL",value:response.data})
-
+export const getTutorialValue = (id) => {
+  return (dispatch) => {
+    EventDataService.get(id)
+      .then((response) => {
+        console.log(response.data, "action");
+        dispatch({ type: "GET_TUTORIAL", value: response.data });
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
-}
-}
+  };
+};
 
-export const changeTitle=(getValue)=>{
-    return{
-        type:"CHANGE_TITLE",value:getValue
-      
-        }
-    }
-    export const changeDescription=(getValue)=>{
-        return{
-            type:"CHANGE_DESCRIPTION",value:getValue
-          
-            }
-        }
+export const changeTitle = (getValue) => {
+  return {
+    type: "CHANGE_TITLE",
+    value: getValue,
+  };
+};
+export const changeDescription = (getValue) => {
+  return {
+    type: "CHANGE_DESCRIPTION",
+    value: getValue,
+  };
+};
 
-        export const changeAvailable=(getValue)=>{
-            return{
-                type:"CHANGE_AVAILABLE",value:getValue
-              
-                }
-            }
+export const changeAvailable = (getValue) => {
+  return {
+    type: "CHANGE_AVAILABLE",
+    value: getValue,
+  };
+};
 
-            export const updateBookingValue=(id,data)=>{
-                return(dispatch)=>{
-                    TutorialDataService.update(id, data)
-                .then(response => {
+export const updateBookingValue = (id, data) => {
+  return (dispatch) => {
+    EventDataService.update(id, data)
+      .then((response) => {
+        console.log(response.data);
+        dispatch({ type: "UPDATE_BOOKING", value: response.data });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+};
 
-                    console.log(response.data);
-                    dispatch({type:"UPDATE_BOOKING",value:response.data})
-            
-                  })
-                  .catch(e => {
-                    console.log(e);
-                  });
-            }
-            }
-
-            export const updateTutorialValue=(id,currentTutorial)=>{
-                return(dispatch)=>{
-                    TutorialDataService.update(id, currentTutorial)
-                .then(response => {
-
-                    console.log(response.data);
-                    dispatch({type:"UPDATE_TUTORIAL_VALUE",value:response.data})
-            
-                  })
-                  .catch(e => {
-                    console.log(e);
-                  });
-            }
-            }
-            export const deleteTutorialValue=(deleteTutorial)=>{
-                return(dispatch)=>{
-                TutorialDataService.delete(deleteTutorial)
-      .then(response => {
-        console.log(response.data,"afterDElete");
-        dispatch({type:"DELETE_TUTORIAL",value:""})
-            }).catch(e => {
-                console.log(e);
-              });
-        }
-
-    }
+export const updateTutorialValue = (id, currentTutorial) => {
+  return (dispatch) => {
+    EventDataService.update(id, currentTutorial)
+      .then((response) => {
+        console.log(response.data);
+        dispatch({ type: "UPDATE_TUTORIAL_VALUE", value: response.data });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+};
+export const deleteTutorialValue = (deleteTutorial) => {
+  return (dispatch) => {
+    EventDataService.delete(deleteTutorial)
+      .then((response) => {
+        console.log(response.data, "afterDElete");
+        dispatch({ type: "DELETE_TUTORIAL", value: "" });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+};
