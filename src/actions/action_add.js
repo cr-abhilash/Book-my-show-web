85285bf2690d8ffdfbb123b55646976d2ac37f91
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 00:28:32 +0530

    removes some comments from action_add and rename newTutorial action to newEvent

--- src/actions/action_add.js
+++ src/actions/action_add.js
@@ -1,60 +1,42 @@
 import TutorialDataService from "../services/tutorial.service";
 
-// export const actionCreate=(tittle)=>{
-//     return{
-//         type:'CHANGE_TITLE',
-//         value:tittle
-//     }
-// }
+export const createTitle = (getValue) => {
+  return {
+    type: "CREATE_TITLE",
+    value: getValue,
+  };
+};
 
-export const createTitle=(getValue)=>{
-    return{
-        type:"CREATE_TITLE",value:getValue
-      
-        }
-    }
-
-    export const createTableValue=(data)=>{
-        return(dispatch)=>{
-            TutorialDataService.create(data)
-      .then(response => {
-        // this.setState({
-        //   available:response.data.available,
-        //   id: response.data.id,
-        //   title: response.data.title,
-        //   description: response.data.description,
-        //   Booking: response.data.Booking,
-
-        //   submitted: true
-        // });
+export const createTableValue = (data) => {
+  return (dispatch) => {
+    TutorialDataService.create(data)
+      .then((response) => {
         console.log(response.data);
-        dispatch({type:"CREATE_TABLE",value:response.data})
+        dispatch({ type: "CREATE_TABLE", value: response.data });
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
-          
-            }
-        }
+  };
+};
 
-        export const newTutorial=()=>{
-            return{
-                type:"NEW_TUTORIAL",value:""
-              
-                }
-            }
+export const newEvent = () => {
+  return {
+    type: "NEW_EVENT",
+    value: "",
+  };
+};
 
-            
-                export const changeDescriptionAnother=(getValue)=>{
-                    return{
-                        type:"CHANGE_DESCRIPTION1",value:getValue
-                      
-                        }
-                    }
-            
-                    export const changeAvailableAnother=(getValue)=>{
-                        return{
-                            type:"CHANGE_AVAILABLE1",value:getValue
-                          
-                            }
-                        }
+export const changeDescriptionAnother = (getValue) => {
+  return {
+    type: "CHANGE_DESCRIPTION1",
+    value: getValue,
+  };
+};
+
+export const changeAvailableAnother = (getValue) => {
+  return {
+    type: "CHANGE_AVAILABLE1",
+    value: getValue,
+  };
+};
