60ef658a453ee71389527ac0e4946ebb869ae02a
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 01:24:42 +0530

    imported all the reducer inside store file

--- src/store.js
+++ src/store.js
@@ -1,1 +1,5 @@
 import { createStore,applyMiddleware,combineReducers } from "redux";
+import reducer from "./reducer/reducer";
+import addNewEvent from "./reducer/addEventReducer";
+import eventListReducer from "./reducer/eventListReducer";
+import eventInfoReducer from "./reducer/eventInfoReducer";
