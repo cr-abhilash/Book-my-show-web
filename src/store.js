a95d719f3eca65ccc1acf6c05c9934655d94d715
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 01:27:00 +0530

    passed all reducers inside combineReducers and assigned to rootReducer var indise store file

--- src/store.js
+++ src/store.js
@@ -4,3 +4,11 @@ import addNewEvent from "./reducer/addEventReducer";
 import eventListReducer from "./reducer/eventListReducer";
 import eventInfoReducer from "./reducer/eventInfoReducer";
 import thunk from 'redux-thunk';
+
+const rootReducer=combineReducers({
+    r1:reducer,
+      addNewEvent:addNewEvent,
+      eventListReducer:eventListReducer,
+      eventInfoReducer:eventInfoReducer
+    })
+  
