ef4c0d90ab9ede3b142501ed36bb2d08295a28a6
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 01:31:12 +0530

    removed the root reducerand some other code inside index component

--- src/index.js
+++ src/index.js
@@ -2,19 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const rootReducer=combineReducers({
-  r1:reducer,
-    r2:reducer1,
-      r3:reducer2,
-      r4:reducer4
-  })
-
-
-const store=createStore(rootReducer,applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
