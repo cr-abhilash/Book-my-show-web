574345e7649f727880789910ac4f07707835bc43
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 01:28:37 +0530

    passed the rootReducer, applymiddleware and thunk and assigned inside store and export the store

--- src/store.js
+++ src/store.js
@@ -1,14 +1,16 @@
-import { createStore,applyMiddleware,combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import reducer from "./reducer/reducer";
 import addNewEvent from "./reducer/addEventReducer";
 import eventListReducer from "./reducer/eventListReducer";
 import eventInfoReducer from "./reducer/eventInfoReducer";
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 
-const rootReducer=combineReducers({
-    r1:reducer,
-      addNewEvent:addNewEvent,
-      eventListReducer:eventListReducer,
-      eventInfoReducer:eventInfoReducer
-    })
-  
+const rootReducer = combineReducers({
+  r1: reducer,
+  addNewEvent: addNewEvent,
+  eventListReducer: eventListReducer,
+  eventInfoReducer: eventInfoReducer,
+});
+
+const store=createStore(rootReducer,applyMiddleware(thunk))
+export default store;
