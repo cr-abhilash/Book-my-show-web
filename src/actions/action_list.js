56a0feb0b31c7f683c708f24694f00299d0ac9b3
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 00:36:38 +0530

    rename imported tutorialDataService to EventDataService inside action_list file

--- src/actions/action_list.js
+++ src/actions/action_list.js
@@ -1,62 +1,60 @@
-import TutorialDataService from "../services/tutorial.service";
+import EventDataService from "../services/eventService";
 
-
-export const getAllValue=()=>{
-return(dispatch)=>{
-    TutorialDataService.getAll()
-  .then(response => {
-    console.log(response.data);
-    dispatch({type:"GET_ALL",value:response.data})
-  })
-  .catch(e => {
-    console.log(e);
-  });
-  
-}}
-
-
-export const findByTitleValue=(titleName)=>{
-    console.log(titleName)
-    return(dispatch)=>{
-        TutorialDataService.findByTitle(titleName)
-      .then(response => {
-        console.log(response.data,"data");
-        dispatch({type:"GET_BY_TITLE",value:response.data})
-      })
-      .catch(e => {
-        console.log(e);
-      });
-      
-    }}
-    export const removeAllValue=()=>{
-        return(dispatch)=>{
-            TutorialDataService.deleteAll()
-      .then(response => {
+export const getAllValue = () => {
+  return (dispatch) => {
+    EventDataService.getAll()
+      .then((response) => {
         console.log(response.data);
-        dispatch({type:"DELETE_ALL",value:response.data})
+        dispatch({ type: "GET_ALL", value: response.data });
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
-          
-        }}
+  };
+};
 
-        export const changeTutorial=(getValue)=>{
-            return{
-    
-                type:'CHANGE_TUTORIAL',
-                value:getValue
-            }}   
-          export const changeIndex=(getValue)=>{
-                return{
-        
-                    type:'CHANGE_INDEX',
-                    value:getValue
-                }}         
-    
-                export const searchTitleValue=(getValue)=>{
-                    return{
-            
-                        type:'SEARCH_TITLE',
-                        value:getValue
-                    }}         
+export const findByTitleValue = (titleName) => {
+  console.log(titleName);
+  return (dispatch) => {
+    EventDataService.findByTitle(titleName)
+      .then((response) => {
+        console.log(response.data, "data");
+        dispatch({ type: "GET_BY_TITLE", value: response.data });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+};
+export const removeAllValue = () => {
+  return (dispatch) => {
+    EventDataService.deleteAll()
+      .then((response) => {
+        console.log(response.data);
+        dispatch({ type: "DELETE_ALL", value: response.data });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+};
+
+export const changeTutorial = (getValue) => {
+  return {
+    type: "CHANGE_TUTORIAL",
+    value: getValue,
+  };
+};
+export const changeIndex = (getValue) => {
+  return {
+    type: "CHANGE_INDEX",
+    value: getValue,
+  };
+};
+
+export const searchTitleValue = (getValue) => {
+  return {
+    type: "SEARCH_TITLE",
+    value: getValue,
+  };
+};
