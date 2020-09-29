90bddc4a81b73ccd7dc681fb6a22f923bd51ae04
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 01:30:11 +0530

    removed the some of imported file inside index compomnent

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { createStore,applyMiddleware,combineReducers } from "redux";
 import { Provider } from "react-redux";
-import reducer from "./reducer/reducer";
-import reducer1 from "./reducer/add-tutorial.component.reducer";
-import reducer2 from "./reducer/tutorial-list.component.reduce";
-import reducer4 from "./reducer/tutorial-component.reducer";
-
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import thunk from 'redux-thunk';
 
 const rootReducer=combineReducers({
   r1:reducer,
