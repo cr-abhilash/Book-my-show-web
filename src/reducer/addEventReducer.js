3a90fd268eebf5a07485d4c81eaae643fd9119da
Rahul Bisht <rahul.bisht@mountblue.tech>
2020-09-30 01:06:33 +0530

    renames the all tutorial name to event inside addEventReducer file

--- src/reducer/addEventReducer.js
+++ src/reducer/addEventReducer.js
@@ -1,64 +1,54 @@
-const initialState={
+const initialState = {
     id: null,
     title: "",
-    description: "", 
+    description: "",
     available: "",
     Booking: false,
-
-    submitted: false
-}
-
-const reducer=(state=initialState,action)=>{
-    switch(action.type){
-        case 'CREATE_TITLE':
-        {
-        return{...state,
-            title: action.value
-        }
-    }
-        case 'CREATE_TABLE':
-        {
-        return{...state,
-            available:action.value.available,
+  
+    submitted: false,
+  };
+  
+  const addEvent = (state = initialState, action) => {
+    switch (action.type) {
+      case "CREATE_TITLE": {
+        return { ...state, title: action.value };
+      }
+      case "CREATE_TABLE": {
+        return {
+          ...state,
+          available: action.value.available,
           id: action.value.id,
           title: action.value.title,
           description: action.value.description,
           Booking: action.value.Booking,
-
-          submitted: true
-        }
-    }
-    
-    case 'NEW_TUTORIAL':
-        {
-        return{...state,
-            id:null,
-      title:"",
-      description:"",
-      Booking:false,
-
-      submitted:false
-
-        }
-    }
-    
-
-case 'CHANGE_AVAILABLE1':
-    {
-    return{...state,
-        available: action.value
-    }
-}
-case 'CHANGE_DESCRIPTION1':{
-    return{...state,
-        description: action.value
-    }
-}
-    
-    default:
+  
+          submitted: true,
+        };
+      }
+  
+      case "NEW_EVENT": {
+        return {
+          ...state,
+          id: null,
+          title: "",
+          description: "",
+          Booking: false,
+  
+          submitted: false,
+        };
+      }
+  
+      case "CHANGE_AVAILABLE1": {
+        return { ...state, available: action.value };
+      }
+      case "CHANGE_DESCRIPTION1": {
+        return { ...state, description: action.value };
+      }
+  
+      default:
         return state;
     }
-}
-
-    
-export default reducer;
+  };
+  
+  export default addEvent;
+  
