73aaf16622f86496245d3deedcd7287670872d10
Abhilash CR <abhilash.cr@mountblue.tech>
2020-10-01 22:40:50 +0530

    featching movies data from backend

--- src/App.js
+++ src/App.js
@@ -9,14 +9,42 @@ import TutorialsList from "./components/tutorials-list.component";
 import HomePage from "./components/HomePage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Header/Footer";
+import TutorialDataService from "./services/tutorial.service";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    //this.retrieveTutorials = this.retrieveTutorials.bind(this);
+    this.state = {
+      moviesData: [],
+    };
+  }
+  componentDidMount() {
+    this.retrieveTutorials();
+  }
+  retrieveTutorials = () => {
+    TutorialDataService.getAll()
+      .then((response) => {
+        this.setState({
+          moviesData: response.data,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
   render() {
     return (
       <div>
         <Header />
         <div className="BodyContent">
           <Switch>
-            <Route exact path={["/", "/homePage"]} component={HomePage} />
+            <Route
+              exact
+              path={["/", "/homePage"]}
+              render={(props) => (
+                <HomePage {...props} movies={this.state.moviesData} />
+              )}
+            />
             <Route exact path="/tutorials" component={TutorialsList} />
             <Route exact path="/add" component={AddTutorial} />
             <Route path="/tutorials/:id" component={Tutorial} />
